32246c2779d76a2d4301b1c1c42f9f682d50e31a
Rob Bovill <rbovill@lsst.org>
2021-09-17 16:13:05 -0700

    Moved the auxtel_visit script functionality to a Class in a Python module.
    Renamed ScriptQueue_Controller.py module to script_queue_controller.py.
    
    Removed all the controller definitions from test_auxtel_visit.py. Now using the IntegrationTests ScriptQueueController Class. Testing that the integration test script is added to the queue.
    
    Put the heartbeat loop into a try/except block.
    
    Removed the remote.start_task call from aux_tel_visit.py module; it is redundant in the async with block.

--- bin/auxtel_visit.py
+++ bin/auxtel_visit.py
@@ -22,32 +22,8 @@
 
 import asyncio
 
-from lsst.ts import IntegrationTests
-from lsst.ts import salobj
-from lsst.ts.idl.enums import ScriptQueue
+from lsst.ts.IntegrationTests import AuxTelVisit
 
+script_class = AuxTelVisit()
 
-async def amain():
-    async with salobj.Domain() as domain, salobj.Remote(
-        domain=domain, name="ScriptQueue", index=2
-    ) as remote:
-        # note: use index=1 for MT, 2 for AuxTel;
-        # also since `async with` is used,
-        # you do NOT have to wait for the remote to start
-        configuration = IntegrationTests.auxtel_visit_config()
-        print("Test configuration:\n" + configuration)
-        await remote.start_task
-        await remote.evt_heartbeat.next(flush=True, timeout=30)
-        await remote.cmd_pause.start(timeout=10)
-        await remote.cmd_add.set_start(
-            timeout=10,
-            isStandard=True,
-            path="auxtel/take_image_latiss.py",
-            config=configuration,
-            logLevel=10,
-            location=ScriptQueue.Location.FIRST,
-        )
-        await remote.cmd_resume.set_start(timeout=10)
-
-
-asyncio.run(amain())
+asyncio.run(script_class.run())
